7413953306313d65166d39b031b6002e0ac83eb1
SeanTheeAwesome <sean.kennedy.obrien@gmail.com>
2017-05-12 13:27:38 -0400

    Add files via upload

---
+++ Chapter 1 Test and Final Project/Chapter 1 Test and Final Project/Chapter 1 Test and Final Project.cpp
@@ -0,0 +1,26 @@
+// Chapter 1 Test and Final Project.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+//Initializing the function, so it can be called. 
+void runPrint() {
+	std::cout << "This is the Print function" << std::endl;
+
+}
+
+
+
+
+//Program starts here
+int main()
+{
+	std::cout << "This is the start of main" << std::endl;
+	runPrint();
+	std::cout << "This is the end of main" << std::endl;
+
+
+    return 0;
+}
+
